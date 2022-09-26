11b7339e05acd4d5608bd0543ee5df902d301769
xXRimaskXx <111510470+xXRimaskXx@users.noreply.github.com>
2022-09-26 15:24:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js')
+const client = new Discord.Client(
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"],}
+)
+
+client.login("MTAyMjUwOTE0Mzg3MDk0NzQwOQ.G320_w.UOlqdcsarkpdWEWWnz-Y7Itx-VZRBtBgL91ZPM")
+
+const ytch = require("yt-channel-info")
+
+const fs = require("fs");
+
+client.commands = new Discord.Collection();
+
+
+const commandsFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commandsFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const commandsFolder = fs.readdirSync("./commands");
+for (const folder of commandsFolder) {
+    const commandsFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith(".js"));
+    for (const file of commandsFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
+
+const eventsFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+for (const file of eventsFiles) {
+    const event = require(`./events/${file}`);
+    client.on(event.name, (...args) => event.execute(...args))
+}
+
+client.on("message", message => {
+    const prefix = "!";
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command) && !client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command))) return
+
+    var comando = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command))
+
+    if (comando.onlyStaff) {
+        if (!message.member.hasPermission("ADMINISTRATOR")) {
+            message.channel.send("Non hai il permesso di eseguire questo comando")
+            return
+        }
+    }
+
+    comando.execute(message, args);
+})
